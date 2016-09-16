495976c3c5ab94dbad5a96ac98602edec19d1b95
Rebecca Allen <rebeccazarsky@gmail.com>
2016-09-16 11:33:32 -0400

    add twigs and partials, some of the required set up for app.php, centering in styles.css

---
+++ app/app.php
@@ -0,0 +1,15 @@
+<?php
+
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/RepeatCounter.php';
+    date_default_timezone_set('America/New_York');
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+
+
+
+    return $app;
+?>
