01cea3c5f02c6a0b0596b9eb2101bb9c98d94428
Rebecca Allen <rebeccazarsky@gmail.com>
2016-09-16 16:31:45 -0400

    add a little more styling, adjustments to app.php and countRepeats function, still buggy, needs more work, not finished

--- app/app.php
+++ app/app.php
@@ -1,14 +1,16 @@
 <?php
 
-    require_once __DIR__.'/../vendor/autoload.php';
-    require_once __DIR__.'/../src/RepeatCounter.php';
+    require_once(__DIR__.'/../vendor/autoload.php');
+    require_once(__DIR__.'/../src/RepeatCounter.php');
     date_default_timezone_set('America/New_York');
 
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-
+    $app->get("/", function() use($app) {
+        return $app['twig']->render('home.html.twig');
+    });
 
 
     return $app;
