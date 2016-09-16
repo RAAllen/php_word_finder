4890ae0b42c1551cb2d92b2f60703c0e125e6420
Rebecca Allen <rebeccazarsky@gmail.com>
2016-09-16 10:46:14 -0400

    add missing semicolons in tests, some unfinished logic for RepeatCounter class

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,7 +1,24 @@
 <?php
     class RepeatCounter {
-        function countRepeats() {
 
+        public $word;
+        public $sentence;
+
+        function countRepeats($word, $sentence)
+        {
+            $word_count = 0;
+            $lowered_word = strtolower($word);
+            $lowered_sentence = strtolower($sentence);
+            $split_sentence = explode(' ', $lowered_sentence);
+            // $split_sentence = multiexplode(array(' ', '.', '!', '?', '""', "''", ':',), $lowered_sentence);
+
+            for($index = 0; $index < count($split_sentence); $index++)
+            {
+                if($lowered_word === $split_sentence[$index])
+                {
+                    $word_count = ($word_count + 1);
+                }
+            }
         }
     }
 ?>
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -1,105 +1,105 @@
 <?php
     require_once "src/RepeatCounter.php";
 
-    class RepeatCounter extends PHPUnit_Framework_TestCase {
+    class RepeatCounterTest extends PHPUnit_Framework_TestCase {
 
         function test_1letter_1word_1time() {
             //Arrange
             $test_RepeatCounter = new RepeatCounter;
             $word = "I";
-            $sentence = "I."
+            $sentence = "I.";
             //Act
-            $result = $test_RepeatCounter->countRepeats($word, $sentence);
+            $word_count = $test_RepeatCounter->countRepeats($word, $sentence);
             //Assert
-            $this->assertEquals(1, $result);
+            $this->assertEquals(1, $word_count);
         }
 
         function test_letters_1word_1time() {
             //Arrange
             $test_RepeatCounter = new RepeatCounter;
             $word = "Cats";
-            $sentence = "Cats!"
+            $sentence = "Cats!";
             //Act
-            $result = $test_RepeatCounter->countRepeats($word, $sentence);
+            $word_count = $test_RepeatCounter->countRepeats($word, $sentence);
             //Assert
-            $this->assertEquals(1, $result);
+            $this->assertEquals(1, $word_count);
         }
 
         function test_1letter_words_1time() {
             //Arrange
             $test_RepeatCounter = new RepeatCounter;
             $word = "I";
-            $sentence = "I love cats."
+            $sentence = "I love cats.";
             //Act
-            $result = $test_RepeatCounter->countRepeats($word, $sentence);
+            $word_count = $test_RepeatCounter->countRepeats($word, $sentence);
             //Assert
-            $this->assertEquals(1, $result);
+            $this->assertEquals(1, $word_count);
         }
 
         function test_letters_words_1time() {
             //Arrange
             $test_RepeatCounter = new RepeatCounter;
             $word = "cats";
-            $sentence = "My favorite animals are cats."
+            $sentence = "My favorite animals are cats.";
             //Act
-            $result = $test_RepeatCounter->countRepeats($word, $sentence);
+            $word_count = $test_RepeatCounter->countRepeats($word, $sentence);
             //Assert
-            $this->assertEquals(1, $result);
+            $this->assertEquals(1, $word_count);
         }
 
         function test_1letter_words_times() {
             //Arrange
             $test_RepeatCounter = new RepeatCounter;
             $word = "a";
-            $sentence = "I went to a pet store to get a bag of cat food.."
+            $sentence = "I went to a pet store to get a bag of cat food..";
             //Act
-            $result = $test_RepeatCounter->countRepeats($word, $sentence);
+            $word_count = $test_RepeatCounter->countRepeats($word, $sentence);
             //Assert
-            $this->assertEquals(2, $result);
+            $this->assertEquals(2, $word_count);
         }
 
         function test_letters_words_times() {
             //Arrange
             $test_RepeatCounter = new RepeatCounter;
             $word = "the";
-            $sentence = "I think the easiest way to make friends with the neighborhood cats is to give them the finest tuna treats."
+            $sentence = "I think the easiest way to make friends with the neighborhood cats is to give them the finest tuna treats.";
             //Act
-            $result = $test_RepeatCounter->countRepeats($word, $sentence);
+            $word_count = $test_RepeatCounter->countRepeats($word, $sentence);
             //Assert
-            $this->assertEquals(3, $result);
+            $this->assertEquals(3, $word_count);
         }
 
         function test_ignore_case() {
             //Arrange
             $test_RepeatCounter = new RepeatCounter;
             $word = "cats";
-            $sentence = "Cats are totally awesome."
+            $sentence = "Cats are totally awesome.";
             //Act
-            $result = $test_RepeatCounter->countRepeats($word, $sentence);
+            $word_count = $test_RepeatCounter->countRepeats($word, $sentence);
             //Assert
-            $this->assertEquals(1, $result);
+            $this->assertEquals(1, $word_count);
         }
 
         function test_find_contraction() {
             //Arrange
             $test_RepeatCounter = new RepeatCounter;
             $word = "you're";
-            $sentence = "You're my favorite cat."
+            $sentence = "You're my favorite cat.";
             //Act
-            $result = $test_RepeatCounter->countRepeats($word, $sentence);
+            $word_count = $test_RepeatCounter->countRepeats($word, $sentence);
             //Assert
-            $this->assertEquals(1, $result);
+            $this->assertEquals(1, $word_count);
         }
 
         function test_find_word_in_contraction() {
             //Arrange
             $test_RepeatCounter = new RepeatCounter;
             $word = "you";
-            $sentence = "You're my favorite cat."
+            $sentence = "You're my favorite cat.";
             //Act
-            $result = $test_RepeatCounter->countRepeats($word, $sentence);
+            $word_count = $test_RepeatCounter->countRepeats($word, $sentence);
             //Assert
-            $this->assertEquals(1, $result);
+            $this->assertEquals(1, $word_count);
         }
 
     }
